d0987400f2d81dd6f913c00d64901c54a055625a
nicneem <hiroyuki.t30@gmail.com>
2022-08-07 16:55:09 +0900

    テストコード　終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
+         validates :nickname, presence: true  
+         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+         validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
          validates :date_of_birth, presence: true
-
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         validates :password, format: {with: VALID_PASSWORD_REGEX}
          
          has_many :products 
          has_many :purchases
