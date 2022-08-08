6b35982ce8a80f3a1c82a7163d691e9c1e5318c8
nicneem <hiroyuki.t30@gmail.com>
2022-08-08 17:25:35 +0900

    商品出品情報を保存するためのモデルとテーブル

--- Gemfile
+++ Gemfile
@@ -31,9 +31,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 4.0.0'
-  gem 'factory_bot_rails'
-  gem 'pry-rails'
+  
   gem 'faker'
 end
 
@@ -61,3 +59,7 @@ gem 'rubocop', require: false
 
 end
 gem 'devise'
+gem 'active_hash'
+gem 'rspec-rails', '~> 4.0.0'
+gem 'factory_bot_rails'
+gem 'pry-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_123109) do
+ActiveRecord::Schema.define(version: 2022_08_08_060818) do
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.text "product_description", null: false
+    t.integer "category_id", null: false
+    t.integer "commodity_condition_id", null: false
+    t.integer "burden_of_shipping_charges_id", null: false
+    t.integer "shipping_area_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_08_04_123109) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
